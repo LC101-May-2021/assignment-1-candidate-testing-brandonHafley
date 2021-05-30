88d8ff2ebd5dc07f79b0c5901645838914ed5e9e
Brandon Hafley <bchafley@gmail.com>
2021-05-30 15:10:22 +0000

    Slight adjustments. Hope to pass

--- candidate-testing.js
+++ candidate-testing.js
@@ -14,7 +14,7 @@ let candidateAnswer = '';
 // question and answer matches
 let questions = [
     'Who was the first American woman in space? ',
-    'True or false: 5 kilometers == 5000 meters? ',
+    'True or false: 5 kilometer == 5000 meters? ',
     '(5 + 3) / 2 * 10 = ? ',
     'Given the array [8, \'Orbit\', \'Trajectory\', 45], what entry is at index 2? ',
     'What is the minimum crew size for the ISS? '
@@ -76,7 +76,7 @@ function gradeQuiz(candidateAnswers) {
     }
 
     grade = `>>> Overall Grade: ${gradingEquation}% (${numOfCorrectAnswers} of ` +
-        `${questions.length} responses correct) <<<\n>>> Status: ${status} <<<`;
+        `${questions.length} responses correct) <<< \n>>> Status: ${status} <<<`;
 
     return grade;
 }
